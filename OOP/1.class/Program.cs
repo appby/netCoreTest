7f90dc20703301a62cdbb441ee565edf49eb64cd
Mert <mgencer@sigortam.net>
2022-05-06 14:03:49 +0300

    Add oop folder & first release

---
+++ OOP/1.class/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace sample1
+{
+    class Student
+    {
+        public int studentNo { get; set; }
+        public string? studentName { get; set; }
+        public string? studentBranch { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student()
+            {
+                studentNo = 1,
+                studentName = "John",
+                studentBranch = "11-A"
+            };
+
+            Student[] students = new Student[1];
+            students[0]=student1;
+            foreach (var item in students)
+            {
+                Console.WriteLine($"Ogrenci No: {item.studentNo}\nOgrenci Adi: {item.studentName}\nOgrenci Sube: {item.studentBranch}");
+            }
+        }
+    }
+}
